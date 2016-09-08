ee0919edd201782df8113e1144701736b79f5ea1
Chris Lorton <chrislorton@outlook.com>
2016-09-08 18:13:45 +0100

    removed source map

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', ['watch']);
 
 gulp.task('sass', function () {
  return gulp.src('./src/sass/style.scss')
-  .pipe(sourcemaps.init())
   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-  .pipe(sourcemaps.write())
   .pipe(gulp.dest('./dist/css'));
 });
 
