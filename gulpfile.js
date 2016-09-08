23605d7a253a80cffe168534787126959b4cde4e
Chris Lorton <chrislorton@outlook.com>
2016-09-08 16:32:03 +0100

    Added compression to SASS. Added source maps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', ['watch']);
 
 gulp.task('sass', function () {
  return gulp.src('./src/sass/style.scss')
-  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.init())
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest('./dist/css'));
 });
 
